0da87ff094a0d5cad296b83174a0a7f53a5bfa94
MihailPetkov96 <mpetkov228@gmail.com>
2025-01-09 17:58:59 +0200

    Complete exercise 9.24 Patientor, step 4

--- src/components/PatientPage/index.tsx
+++ src/components/PatientPage/index.tsx
@@ -15,11 +15,26 @@ const PatientPage = ({ patients }: Props) => {
     return null;
   }
 
+  console.log(patient.entries);
+
   return (
     <div>
       <h2>{patient.name} <GenderIcon gender={patient.gender} /></h2>
       <div>ssn: {patient.ssn}</div>
       <div>occupation: {patient.occupation}</div>
+      <h3>entries</h3>
+      <div>
+        {patient.entries.map(entry => 
+          <div key={entry.id}>
+            <div>{entry.date} {entry.description}</div>
+            <ul>
+              {entry.diagnosisCodes?.map(code => 
+                <li key={code}>{code}</li>
+              )}
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
--- src/types.ts
+++ src/types.ts
@@ -10,6 +10,52 @@ export enum Gender {
   Other = "other"
 }
 
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+type Discharge = {
+  date: string,
+  criteria: string
+};
+
+type SickLeave = {
+  startDate: string,
+  endDate: string
+};
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
 export interface Patient {
   id: string;
   name: string;
@@ -17,6 +63,7 @@ export interface Patient {
   gender: Gender;
   ssn?: string;
   dateOfBirth?: string;
+  entries: Entry[]
 }
 
 export type PatientFormValues = Omit<Patient, "id" | "entries">;
